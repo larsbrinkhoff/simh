3a41109fd3191dc3986d5f001e9d62470e7921c6
Lars Brinkhoff <lars@nocrew.org>
2021-03-08 12:18:15 +0100

    Stub.

--- imlac/imlac_defs.h
+++ imlac/imlac_defs.h
@@ -35,45 +35,10 @@
 #define STOP_IBKPT      2
 #define STOP_ACCESS     3
 
-#define FLAG_PTR     010000
-#define FLAG_PTP     000400
-#define FLAG_TTY_T   000040
-#define FLAG_KBD     000020
-#define FLAG_TTY_R   000010
-#define FLAG_SYNC    000002
-
-typedef struct {
-  uint16 num;
-  uint16 (*iot)(uint16 insn, uint16 AC);
-  const char *mnemonics[8];
-} SUBDEV;
-
-typedef struct {
-  int codes;
-  SUBDEV subdev[4];
-} IMDEV;
-
 extern t_bool build_dev_tab (void);
-extern void flag_on (uint16 flag);
-extern void flag_off (uint16 flag);
-extern uint16 flag_check (uint16 flag);
-extern void dp_on (int flag);
-extern uint16 dp_is_on (void);
-extern void crt_point (uint16 x, uint16 y);
-extern void crt_line (uint16 x1, uint16 y1, uint16 x2, uint16 y2);
-extern void crt_idle (void);
-extern void crt_hvc (void);
-extern void rom_data (uint16 *data);
-extern void rom_tty (void);
-extern void rom_stty (void);
-extern void rom_ptr (void);
 
 extern REG cpu_reg[];
 extern uint16 M[];
-extern uint16 memmask;
-extern SUBDEV *dev_tab[0100];
-extern DEVICE cpu_dev, irq_dev, rom_dev, dp_dev, crt_dev, kbd_dev;
-extern DEVICE tty_dev, ptr_dev, ptp_dev, sync_dev;
-extern DEVICE bel_dev;
+extern DEVICE cpu_dev;
 
 #endif /* IMLAC_DEFS_H_ */
